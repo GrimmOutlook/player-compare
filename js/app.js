15fc2c3abf5eac9d2dcb7e01544fd3ef81fa8a17
GrimmOutlook <grimmoutlook87@gmail.com>
2017-05-03 19:47:11 -0400

    Player lists for dropdown menus populate from API and are sorted alphabetically.

--- js/app.js
+++ js/app.js
@@ -22,33 +22,72 @@ function getScoringFromAPI(year, week, position, callback){
   $.ajax(settings);
 }
 
-// Callback Fxn that displays the player in a dropdown menu for user selection
-function displayDropdown(data){
+// Callback Fxn that displays a list of players in a dropdown menu for user selection for both Player 1 and Player 2.
+function displayDropdown(playerData){
   var player = {};
+  var players = [];
   var resultElement = '';
+  var selectedPosition = Object.keys(playerData.positions)[0];
+  var selectedPlayers = playerData.positions[Object.keys(playerData.positions)[0]];
+  debugger
 
-  for(var i = 0; i < data.positions.QB.length; i++){
+  for(var i = 0; i < selectedPlayers.length; i++){
     player = {
-      name: data.positions.QB[i].firstName + ' ' + data.positions.QB[i].lastName,
-      team: data.positions.QB[i].teamAbbr
+      name: selectedPlayers[i].firstName + ' ' + selectedPlayers[i].lastName,
+      team: selectedPlayers[i].teamAbbr,
+      alpha: selectedPlayers[i].lastName
     };
 
-    resultElement += '<option>' + player.name + ',  ' + player.team + '</option>'
+    players.push(player);
+
   }
 
-  $('.practice').html(resultElement);
+  players.sort(alphaSort);
+  debugger
+
+  for(var x = 0; x < players.length; x++){
+    resultElement += '<option>' + players[x].name + ',  ' + players[x].team + '</option>'
+  }
+
+  $('.player-list').html(resultElement);
 
 }
 
-function watchSubmit(){
+// Sort names alphabetically
+function alphaSort(a, b) {
+    var playerA = a.alpha.toUpperCase();
+    var playerB = b.alpha.toUpperCase();
+
+    var comparison = 0;
+      if (playerA > playerB) {
+        comparison = 1;
+      } else if (playerA < playerB) {
+        comparison = -1;
+      }
+      return comparison;
+  }
+
+function pickPosition(){
   $('#position-choice').click(function(e){
     debugger
     e.preventDefault();
     var position = $(this).find('option:selected').val();
+    debugger
     getScoringFromAPI("2016", "1", position, displayDropdown);
   });
 }
 
-$(function(){watchSubmit();});
+// function selectCompare(){
+//   $('.compare-initial').click(function(e){
+//     debugger
+//     e.preventDefault();
+//     var playerOne =
+//     var playerTwo =
+//     var year = $(this).find('option:selected').val();
+//     getScoringFromAPI(year, "1", position, displayDropdown);
+//   });
+// }
+
+$(function(){pickPosition();});
 
 
