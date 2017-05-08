1b86b827af41b8fab71d1adc4d6a29e1dbe5a32b
GrimmOutlook <grimmoutlook87@gmail.com>
2017-05-08 17:17:37 -0400

    Begin use of player & year state.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var endpointURLScoring = "http://api.fantasy.nfl.com/v1/players/scoringleaders";
 var endpointURLPhoto = "http://api.fantasy.nfl.com/v1/players/weekvideos";
 
-// var state = {
-//   player: '',
-//   year: ''
-//   , week: ''
-// };
+var state = {
+  player: '',
+  year: ''
+  // , week: ''
+};
 
 
 // ---------------------------  API Call fxns.  ---------------------------------
@@ -89,23 +89,9 @@ function alphaSort(a, b) {
 
 
 function displayStats(statData){
-  debugger
-  // var playerOneInfo = {};
-  // debugger
-  var x = relevantInfo(playerOneInfo);
-  debugger
-  // var playerTwoInfo = {};
-  console.log(playerOneInfo);
-  console.log(x);
-  // console.log(playerTwoInfo);
-  debugger
-  var selectedPosition = Object.keys(statData.positions)[0];
-  var selectedPlayerOne = statData.positions[Object.keys(statData.positions)[0]];
-  // var selectedYearOne = ;
-  // var selectedPlayerTwo = ;
-  // var selectedYearTwo = ;
 
-  // console.log(playerOne);
+  var selectedPosition = Object.keys(statData.positions)[0];
+  var selectedPlayer = statData.positions[Object.keys(statData.positions)[0]];
 
   var testingDynamicInput = '<div>' + selectedPlayerOne.firstName + ' ' + selectedPlayerOne.lastName + '</div>';
 
@@ -113,36 +99,35 @@ function displayStats(statData){
   debugger
   var testingOutputTwo = '<div>' + selectedPlayers[16].firstName + ' ' + selectedPlayers[16].lastName + '</div>';
 
-  // console.log(testingOutputOne);
-  // console.log(testingOutputTwo);
 
-  $('#player-one-stuff').html(testingOutputOne);
-  $('#player-two-stuff').html(testingOutputTwo);
+  $('#player-stat-display').html(testingOutputTwo);
 }
 
-function relevantInfo(position, playerOne, yearOne, playerTwo, yearTwo){
-  var playerOneInfo = {};
-  var playerTwoInfo = {};
+function relevantInfo(position, player, year){
+  var playerInfo = {};
+  // var playerTwoInfo = {};
 
-  var playerOneArray = playerOne.split(' ');
-  var playerOneFN = playerOneArray.shift();
-  var playerOneLN = playerOneArray.shift().slice(0, -1);
-  playerOneInfo = {
-    season: yearOne,
+  var playerArray = player.split(' ');
+  var playerFN = playerArray.shift();
+  var playerLN = playerArray.shift().slice(0, -1);
+  playerInfo = {
+    season: year,
     positions: position,
-    firstName: playerOneFN,
-    lastName: playerOneLN
+    firstName: playerFN,
+    lastName: playerLN
   }
+  debugger
+  console.log(playerInfo);
 
-  var playerTwoArray = playerTwo.split(' ');
-  var playerTwoFN = playerTwoArray.shift();
-  var playerTwoLN = playerTwoArray.shift().slice(0, -1);
-  playerTwoInfo = {
-    season: yearTwo,
-    positions: position,
-    firstName: playerTwoFN,
-    lastName: playerTwoLN
-  }
+  // var playerTwoArray = playerTwo.split(' ');
+  // var playerTwoFN = playerTwoArray.shift();
+  // var playerTwoLN = playerTwoArray.shift().slice(0, -1);
+  // playerTwoInfo = {
+  //   season: yearTwo,
+  //   positions: position,
+  //   firstName: playerTwoFN,
+  //   lastName: playerTwoLN
+  // }
   debugger
 
   // displayStats(playerOneInfo, playerTwoInfo);
@@ -170,10 +155,13 @@ function selectCompare(){
     var playerTwo = $('#player-two').find('option:selected').val();
     var yearTwo = $('#player-two-year').find('option:selected').val();
     debugger
-    // state.player = playerOne;
-    // state.year = yearOne;
-    getStatsFromAPI(position, yearOne, displayStats);
-    relevantInfo(position, playerOne, yearOne, playerTwo, yearTwo);
+    state.player = playerOne;
+    state.year = yearOne;
+    debugger
+    console.log(state);
+    debugger
+    getStatsFromAPI(position, state.year, displayStats);
+    relevantInfo(position, state.player, state.year);
   });
 }
 
